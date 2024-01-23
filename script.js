9d1eadb62d16bf2d2b8b0570c95615edc69c1735
gabriellopes754 <gabriellopes15942@outlook.com>
2024-01-22 22:36:25 -0300

    inicio formulario e JS

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function generatePassword() {
+  const chars =
+    "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+
+  let password = ""
+
+  for (let i = 0; i < 8; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length)
+    password += chars.substring(randomNumber, randomNumber + 1)
+  }
+
+  const inputEl = document.querySelector("#password")
+  inputEl.value = password
+}
+
+generatePassword()
