0bb6e69166227984002084175ae8053690506ca8
gabriellopes754 <gabriellopes15942@outlook.com>
2024-01-27 21:44:52 -0300

    Finalização de eventos

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,26 @@
 let passwordLength = 16
 const inputEl = document.querySelector("#password")
+const maisculasEl = document.querySelector("#maiusculas")
+const numerosEl = document.querySelector("#numeros")
+const simbolosEl = document.querySelector("#simbolos")
 
 function generatePassword() {
-  const chars =
-    "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+  let chars ="abcdefghjkmnpqrstuvwxyz"
+
+    const maisculaschars = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+    const numeroschars = "123456789"
+    const simboloschars = "?!@&*()[]"
+
+
+    if(maisculasEl.checked) {
+      chars += maisculaschars
+    }
+    if(numerosEl.checked) {
+      chars += numeroschars
+    }
+    if(simbolosEl.checked) {
+      chars += simboloschars
+    }
 
   let password = ""
 
@@ -19,14 +36,25 @@ function generatePassword() {
 const passwordLengthEl = document.querySelector("#password-length")
       passwordLengthEl.addEventListener("input", function () {
         passwordLength = passwordLengthEl.value
+        document.querySelector('#comprsenha2').innerText = passwordLength
         generatePassword()
       })
 
-function copy() {
-  navigator.clipboard.writeText(inputEl.value)
-}
+      function copy() {
+        navigator.clipboard.writeText(inputEl.value)
+      }
+
+
+
+maisculasEl.addEventListener("click" , generatePassword)
+numerosEl.addEventListener("click", generatePassword)
+simbolosEl.addEventListener("click", generatePassword)
+
+
 
 document.querySelector('#copy').addEventListener("click", copy)
 document.querySelector('#copy2').addEventListener("click", copy)
+document.querySelector("#atualizar").addEventListener("click", generatePassword)
+
 
 generatePassword()
